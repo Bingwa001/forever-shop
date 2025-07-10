29234fa8bd1f872c52cc8ecb5370a0171dddae3f
Bingwa001 <onebingwa7@gmail.com>
2025-07-11 00:11:52 +0300

    Add latest collection component with product display and context setup

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets';
 
 const Hero = () => {
   return (
@@ -11,16 +12,17 @@ const Hero = () => {
                 <p className='font-medium textt-sm md:text-base'> OUR BESTSELLERS</p>
 
             </div>
-            <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+            <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
             <div className='flex items-center gap-2'>
-                <p className='font-semibold text-sm md:text-base'>SHOP INN</p>
+                <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
                 <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
 
             </div>
 
         </div>
-
         </div>
+        {/* Hero Right Side */}
+        <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
 
     </div>
   )
